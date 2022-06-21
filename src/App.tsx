834bf01136684f78fde19e4de7947a87fac47553
Nadia Ligia <nlnadialigia@gmail.com>
2022-06-20 23:41:11 -0300

    chore($config): add and configure apollo and graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-/* eslint-disable object-curly-spacing */
+import {gql, useQuery} from '@apollo/client';
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
+interface Lesson {
+  id: string,
+  title: string,
+}
 
 function App() {
+  const {data} = useQuery<{lessons: Lesson[];}>(GET_LESSONS_QUERY);
+
   return (
-    <h1 className="text-2xl font-bold text-violet-500">
-      Hello World
-    </h1>
+    <ul>
+      {data?.lessons.map((lesson) => <li key={lesson.id}>{lesson.title}</li>)}
+    </ul>
   );
 }
 
