f0f40d3acdf101eb8925623f3313c298d30751f0
Nadia Ligia <nlnadialigia@gmail.com>
2022-06-20 21:25:23 -0300

    chore($config): add and config tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
